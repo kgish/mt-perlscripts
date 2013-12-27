b8b92391606ab49d36fc07ffa1e767c07d2e9576
Kiffin Gish <kiffin.gish@planet.nl>
2013-12-27 13:50:46 +0100

    Included debugging flag to enable logging

--- create_thumbnails.pl
+++ create_thumbnails.pl
@@ -14,6 +14,7 @@ my $root_path = $cfg->val( 'create_thumbnails', 'root_path' );
 my $doc_dir = $cfg->val( 'create_thumbnails', 'doc_dir' );
 my $img_dir = $cfg->val( 'create_thumbnails', 'img_dir' );
 my $thumbnails_dir = $cfg->val( 'create_thumbnails', 'thumbnails_dir' );
+my $debug = $cfg->val( 'create_thumbnails', 'debug' );
 
 my $doc_path ="$root_path/$doc_dir";
 my $img_path = "$doc_path/$img_dir";
@@ -39,10 +40,13 @@ my $dbh = DBI->connect("dbi:${dbi}:${dbname}",$dbuser,$dbpasswd) or die "Connect
 opendir(DIR, $img_path) or die "cannot open directory: '$img_path'";
 
 my @files = grep(/jpg|png|gif|jpeg/i, readdir(DIR));
+print "Readdir: " . (scalar @files) . " files\n" if $debug;
 @files = shuffle(@files);
 foreach my $file (@files) {
 #foreach my $file (sort grep(/jpg|png|gif|jpeg/i, readdir(DIR))) {
     my $sql = "select entry_authored_on, entry_basename, entry_title from mt_entry where entry_blog_id = 1 and entry_class = 'entry' and entry_status = 2 and entry_text like '" . '%' . $file . '%' . "'";
+    print "\$file='$file'\n" if $debug;
+    # print "\$sql='$sql'\n";
     my $sth = $dbh->prepare($sql);
     $sth->execute or die "SQL Error: $DBI::errstr" . "\$sql='$sql'" . "\n";
     my @entries;
@@ -53,12 +57,14 @@ foreach my $file (@files) {
         my $url = "$year/$month/$basename.html";
         my $html = "$doc_path/$url";
         if (-e $html) {
+            print "\$title='$title',\$url='$url'\n" if $debug;
             push(@entries, join("|", $title, $url));
         }
     }
     next unless @entries;
     $images{$file} = \@entries;
 }
+print "Done: files\n" if $debug;
 
 $dbh->disconnect();
 
@@ -70,7 +76,11 @@ $data{max_width} = $max_width;
 
 my $geometry = "${max_width}x${max_width}";
 
+print "Each images\n" if $debug;
+my $num = 0;
 while( my( $file, $value ) = each %images ){
+    $num++;
+    print "$num: \$file='$file\n" if $debug;
     my $src = Image::Magick->new;
     my $img;
     $src->Read("$img_path/$file");
@@ -78,7 +88,7 @@ while( my( $file, $value ) = each %images ){
     my $thumbnail = "$thumbnails_path/$file";
     $thumb->Write($thumbnail);
     $file = uri_escape($file);
-#    print "file=$file|x=${x}|y=${y}\n";
+    print "\$file='$file',\$x=${x}, \$y=${y}\n" if $debug;
     $img->{name} = $file;
     $img->{x} = $x;
     $img->{y} = $y;
@@ -90,17 +100,22 @@ while( my( $file, $value ) = each %images ){
         my ($title, $url) = split(/\|/, $entry);
         $ent->{title} = $title;
         $ent->{url} = $url;
-#        print "$cnt: title=$title|url=$url\n";
+        print "$cnt: \$title='$title',\$url='$url'\n" if $debug;
         push(@{$img->{entries}}, $ent);
     }
+    print "Push\n" if $debug;
     push(@{$data{images}}, $img);
 }
 
+print "Shuffle: before\n" if $debug;
 @{$data{images}} = shuffle(@{$data{images}});
+print "Shuffle: after\n" if $debug;
 
+print "Open: before\n" if $debug;
 open my $fh, ">", $thumbnails_json or die "Cannot open file '$thumbnails_json' ($!)";
 print $fh to_json(\%data, { utf8 => 1, pretty => 1 } );
 close $fh;
+print "Open: after\n" if $debug;
 
 # --------------------------------------------------------------------------
 # $.getJSON("http://www.kiffingish.com/thumbnails.json", function(data){
